ba0483e513a10a75a052241c4ce571f138a538bc
Federico Ravasio <ravasio.federico@gmail.com>
2013-12-28 19:02:01 +0100

    Removed sample test case.

--- test/runner.cpp
+++ test/runner.cpp
@@ -1,9 +1,5 @@
 #include "gtest/gtest.h"
 
-TEST(TruthTest, OneIsOne) {
-  ASSERT_EQ(1, 1);
-}
-
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
