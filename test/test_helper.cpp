c49ecd2a05acb39afbf7d50d847b3357dd52809e
Federico Ravasio <ravasio.federico@gmail.com>
2013-12-31 12:36:30 +0100

    Added InputKey's PrintTo to the test helper.

--- test/test_helper.cpp
+++ test/test_helper.cpp
@@ -1,9 +1,30 @@
+#include "input_key.hpp"
 #include "point.hpp"
 #include "test_helper.hpp"
 
 #include <string>
 
 namespace green_leaf {
+  void PrintTo(const InputKey& p, std::ostream* os) {
+    std::string str;
+
+    switch(p) {
+      case InputKey::A:      str = std::string("InputKey::A");      break;
+      case InputKey::B:      str = std::string("InputKey::B");      break;
+      case InputKey::Down:   str = std::string("InputKey::Down");   break;
+      case InputKey::Escape: str = std::string("InputKey::Escape"); break;
+      case InputKey::L:      str = std::string("InputKey::L");      break;
+      case InputKey::Left:   str = std::string("InputKey::Left");   break;
+      case InputKey::R:      str = std::string("InputKey::R");      break;
+      case InputKey::Right:  str = std::string("InputKey::Right");  break;
+      case InputKey::Select: str = std::string("InputKey::Select"); break;
+      case InputKey::Start:  str = std::string("InputKey::Start");  break;
+      case InputKey::Up:     str = std::string("InputKey::Up");     break;
+    }
+
+    *os << str;
+  }
+
   void PrintTo(const Point& p, std::ostream* os) {
     *os << p.toString();
   }
--- test/test_helper.hpp
+++ test/test_helper.hpp
@@ -1,7 +1,9 @@
 #include <iostream>
 
 namespace green_leaf {
+  enum class InputKey;
   class Point;
 
+  void PrintTo(const InputKey& p, std::ostream* os);
   void PrintTo(const Point& p, std::ostream* os);
 }
