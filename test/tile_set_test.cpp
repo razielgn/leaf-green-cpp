741bbe0860a377e21254b5a9a7351a5a8281aae1
Federico Ravasio <ravasio.federico@gmail.com>
2014-01-11 13:22:10 +0100

    Refactored TileSet tests.

--- test/tile_set_test.cpp
+++ test/tile_set_test.cpp
@@ -7,37 +7,30 @@
 namespace green_leaf {
   class TileSetTest : public ::testing::Test {
   protected:
-    TileSetTest() {
-      texture_ = new Texture(Vector2(48, 48));
-      tile_set_ = new TileSet(texture_, tile_size_, 1);
-    }
-    ~TileSetTest() {
-      delete texture_;
-      delete tile_set_;
-    }
+    TileSetTest() { }
 
-    const TileSet* tile_set_;
-    Texture* texture_;
+    Texture texture_ = Texture(Vector2(48, 48));
+    const TileSet tile_set_ = TileSet(&texture_, tile_size_, 1);
     const Vector2 tile_size_ = Vector2(16, 16);
   };
 
   TEST_F(TileSetTest, Texture) {
-    EXPECT_EQ(texture_, tile_set_->texture());
+    EXPECT_EQ(&texture_, tile_set_.texture());
   }
 
   TEST_F(TileSetTest, TileSize) {
-    EXPECT_EQ(tile_size_, tile_set_->tile_size());
+    EXPECT_EQ(tile_size_, tile_set_.tile_size());
   }
 
   TEST_F(TileSetTest, RectangleFromCode) {
-    EXPECT_EQ(Rectangle(0, 0, 16, 16), tile_set_->rectangleFromCode(1));
-    EXPECT_EQ(Rectangle(1, 0, 16, 16), tile_set_->rectangleFromCode(2));
-    EXPECT_EQ(Rectangle(2, 0, 16, 16), tile_set_->rectangleFromCode(3));
-    EXPECT_EQ(Rectangle(0, 1, 16, 16), tile_set_->rectangleFromCode(4));
-    EXPECT_EQ(Rectangle(1, 1, 16, 16), tile_set_->rectangleFromCode(5));
-    EXPECT_EQ(Rectangle(2, 1, 16, 16), tile_set_->rectangleFromCode(6));
-    EXPECT_EQ(Rectangle(0, 2, 16, 16), tile_set_->rectangleFromCode(7));
-    EXPECT_EQ(Rectangle(1, 2, 16, 16), tile_set_->rectangleFromCode(8));
-    EXPECT_EQ(Rectangle(2, 2, 16, 16), tile_set_->rectangleFromCode(9));
+    EXPECT_EQ(Rectangle(0, 0, tile_size_), tile_set_.rectangleFromCode(1));
+    EXPECT_EQ(Rectangle(1, 0, tile_size_), tile_set_.rectangleFromCode(2));
+    EXPECT_EQ(Rectangle(2, 0, tile_size_), tile_set_.rectangleFromCode(3));
+    EXPECT_EQ(Rectangle(0, 1, tile_size_), tile_set_.rectangleFromCode(4));
+    EXPECT_EQ(Rectangle(1, 1, tile_size_), tile_set_.rectangleFromCode(5));
+    EXPECT_EQ(Rectangle(2, 1, tile_size_), tile_set_.rectangleFromCode(6));
+    EXPECT_EQ(Rectangle(0, 2, tile_size_), tile_set_.rectangleFromCode(7));
+    EXPECT_EQ(Rectangle(1, 2, tile_size_), tile_set_.rectangleFromCode(8));
+    EXPECT_EQ(Rectangle(2, 2, tile_size_), tile_set_.rectangleFromCode(9));
   }
 }
