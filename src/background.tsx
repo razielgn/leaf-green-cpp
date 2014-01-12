b6bcc83b78ff449b44725dda628c0f66badc5b08
Federico Ravasio <ravasio.federico@gmail.com>
2014-01-12 12:45:49 +0100

    Added Tiled tile sets.

---
+++ src/background.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="background" tilewidth="16" tileheight="16">
+ <image source="background.png" width="320" height="320"/>
+</tileset>
---
+++ src/decorations.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="decorations" tilewidth="16" tileheight="16">
+ <image source="decorations.png" width="160" height="160"/>
+</tileset>
