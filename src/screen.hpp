cd5f16101d4b62e6075864617a9490283d32d1d4
Federico Ravasio <ravasio.federico@gmail.com>
2014-01-25 16:19:47 +0100

    Added Screen interface.

---
+++ src/screen.hpp
@@ -0,0 +1,20 @@
+#ifndef GL_SCREEN_HPP
+#define GL_SCREEN_HPP
+
+namespace green_leaf {
+  class Content;
+  class GameTime;
+  class Graphics;
+  class Input;
+
+  class Screen {
+  public:
+    virtual ~Screen() { };
+
+    virtual void loadContent(const Content* content) = 0;
+    virtual void update(Input* input, const GameTime* game_time) = 0;
+    virtual void draw(const Graphics* graphics) const = 0;
+  };
+}
+
+#endif
