80f1b1cc21563ec9c6fbb063212b0b63d85fed1f
Federico Ravasio <ravasio.federico@gmail.com>
2014-03-23 14:19:56 +0100

    Added missing includes in src/custom_types.hpp

--- src/custom_types.hpp
+++ src/custom_types.hpp
@@ -1,6 +1,9 @@
 #ifndef GL_CUSTOM_TYPES
 #define GL_CUSTOM_TYPES
 
+#include <string>
+#include <utility>
+
 namespace green_leaf {
   typedef std::pair<const std::string, const std::string> message;
 }
